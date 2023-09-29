73316f9f4037b719468688ba381658ee948a597a
Shrey <115035182+Shreyyyyy@users.noreply.github.com>
2023-09-29 10:28:46 +0530

    Update script.js

--- q7_ajax/script.js
+++ q7_ajax/script.js
@@ -1,39 +1,24 @@
-// let httpRequest;
-const httpRequest = new XMLHttpRequest();
-const url =
-  'https://raw.githubusercontent.com/Punithify/punithify.github.io/main/Data/books.json';
-document.getElementById('ajaxButton').addEventListener('click', () => {
-  makeRequest(url);
+const fetchBtn = document.getElementById("fetch-btn");
+const display = document.getElementById("display");
+
+fetchBtn.addEventListener("click", () => {
+    const xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                const books = JSON.parse(xhr.responseText);
+                books.books.forEach((book) => {
+                    const head = document.createElement("h1");
+                    head.textContent = `${book.name} - ${book.author}`;
+                    display.appendChild(head);
+                });
+            } else {
+                console.error("Failed to fetch data from index.json.");
+            }
+        }
+    };
+
+    xhr.open("GET", "index.json", true);
+    xhr.send();
 });
-
-function makeRequest(url) {
-  httpRequest.onreadystatechange = alertContents;
-  httpRequest.open('GET', url, true);
-  httpRequest.send();
-}
-
-function alertContents() {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    if (httpRequest.status === 200) {
-      const response = JSON.parse(httpRequest.responseText);
-      console.log(response);
-      response.map(
-        (book) =>
-          (document.getElementById(
-            'bookList'
-          ).innerHTML += `<div class="card ml-4" style="width: 18rem;">
-      <img src="${book.imageLink}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h3>${book.title}</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="${book.link}">book link</a>
-      </div>
-    </div>`)
-      );
-    } else {
-      alert('There was a problem with the request.');
-    }
-  }
-}
-
-console.log('hello', httpRequest.responseText);
